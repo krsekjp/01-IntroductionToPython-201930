36b6bc4d4e53c3601e0c605e3e86dd9e1505eb91
krsekjp <krsekjp@rose-hulman.edu>
2019-03-04 09:26:29 -0500

    done

--- src/m4_SimpleTurtle_objects.py
+++ src/m4_SimpleTurtle_objects.py
@@ -10,11 +10,11 @@ Also:
   -- ASSIGNING a VALUE to a NAME (VARIABLE).
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
-         Aaron Wilkin, their colleagues, and PUT_YOUR_NAME_HERE.
+         Aaron Wilkin, their colleagues, and Josh Krsek.
 """
 ###############################################################################
 #
-# TODO: 1.
+# DONE: 1.
 #   Yes, that means for YOU to DO things per the following instructions:
 #
 #   On Line 13 above, replace  PUT_YOUR_NAME_HERE  with your OWN name.
@@ -29,7 +29,7 @@ import rosegraphics as rg
 
 ###############################################################################
 #
-# TODO: 2.
+# DONE: 2.
 #   Allow this module to use the  rosegraphics.py  module by marking the
 #     src
 #   folder in this project as a "Sources Root", as follows:
@@ -48,7 +48,7 @@ import rosegraphics as rg
 
 ###############################################################################
 #
-# TODO: 3.
+# DONE: 3.
 #   Run this module.  A window will pop up and Turtles will move around.
 #   After the Turtles stop moving,
 #      ** click anywhere in the window to close the window **.
@@ -87,7 +87,7 @@ window.delay(20)  # Bigger numbers mean slower animation.
 # The next few lines show how to:
 #   - CONSTRUCT (make) a  SimpleTurtle  object and ASSIGN a NAME to the object.
 # -----------------------------------------------------------------------------
-boris = rg.SimpleTurtle()
+boris = rg.SimpleTurtle('square')
 
 # -----------------------------------------------------------------------------
 # The next few lines show how to:
@@ -103,20 +103,21 @@ boris.forward(200)
 #   - Construct a second SimpleTurtle,
 #     set its  pen  and  speed  INSTANCE VARIABLES, and ask it to do things.
 # -----------------------------------------------------------------------------
-natasha = rg.SimpleTurtle('turtle')
-natasha.pen = rg.Pen('red', 30)  # Second argument is the Pen's thickness
-natasha.speed = 5  # Bigger means faster, max is usually about 10
+natasha = rg.SimpleTurtle('blank')
+natasha.pen = rg.Pen('yellow', 10)  # Second argument is the Pen's thickness
+natasha.speed = 3  # Bigger means faster, max is usually about 10
 
 natasha.backward(50)
 natasha.right(90)
 natasha.forward(125)
+natasha.left(17)
 
-natasha.speed = 1  # Now slower
-natasha.go_to(rg.Point(-100, 200))
+natasha.speed = 7  # Now slower
+natasha.go_to(rg.Point(100, 200))
 
 ###############################################################################
 #
-# TODO: 4.
+# DONE: 4.
 #   Add a few more lines of your own code to make one of the existing
 #   SimpleTurtles move some more and/or have different characteristics.
 #
@@ -153,6 +154,9 @@ natasha.go_to(rg.Point(-100, 200))
 #   As always, test by running the module.
 #
 ###############################################################################
+jimmy = rg.SimpleTurtle('turtle')
+jimmy.pen = rg.Pen('green',5)
+jimmy.speed=10
 
 ###############################################################################
 #
