a1820417199ffd1318fb1c5e639b62ac5a4428f4
krsekjp <krsekjp@rose-hulman.edu>
2019-03-04 08:55:57 -0500

    done

--- src/m3_names_and_expressions.py
+++ src/m3_names_and_expressions.py
@@ -1,9 +1,10 @@
+import math
 
 answer = 2 ** 5
 print(answer * 100)
 
 ###############################################################################
-# TODO: 1.
+# DONE: 1.
 #   Read the 2 lines of code ABOVE this _TODO_.  That code:
 #     1. Computes 2 raised to the 5th power,
 #          yielding the object that is the integer 32.
@@ -20,7 +21,7 @@ print(answer * 100)
 ###############################################################################
 
 ###############################################################################
-# TODO: 2.
+# DONE: 2.
 #   Immediately below this _TODO_, write code that:
 #     - Computes 77 plus the cosine of 2.75.
 #         HINT: You will need to import the   math  module (library).
@@ -28,15 +29,16 @@ print(answer * 100)
 #     - Prints the square root of that computed value.
 #   Run your code (fix errors as needed), then change the above _TODO_ to DONE.
 ###############################################################################
-
+op1= 77 + math.cos(2.75)
+print(math.sqrt(op1))
 ###############################################################################
-# TODO: 3.
+# DONE: 3.
 #   Immediately below this _TODO_, write code that computes and prints:
 #      the square root of ((41 * 88) + (4 * the cosine of 2))
 #   Use as few or as many intermediate names as you feel appropriate.
 #   Run your code (fix errors as needed), then change the above _TODO_ to DONE.
 ###############################################################################
-
+print(math.sqrt(41*88 + 4*math.cos(2)))
 ###############################################################################
 # TODO: 4.
 #   Immediately below this _TODO_,
